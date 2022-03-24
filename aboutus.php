af50e8b99a13e1555cd0b85164d75dbd09ab3fdf
yxnzhe <yanzhe2003@gmail.com>
2022-03-24 23:51:01 +0800

    add user function

--- aboutus.php
+++ aboutus.php
@@ -1,5 +1,6 @@
 <head><?php require_once "navbar.php"; ?></head>
 <body>
     <h1>About Us</h1>
+    <a href="https://github.com/yxnzhe/ElectricBillCalculator">GitHub Repo</a>
 </body>
 <footer><?php require_once "footer.php"; ?></footer>
--- calculator.php
+++ calculator.php
@@ -1,5 +1,6 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
     <?php 
         require_once "navbar.php";
         require_once "count.php";
@@ -10,18 +11,20 @@
         $_SESSION["userUsage"] = 0;
         $totalAmount = 0;
         $totalUsage = 0;
-        $pax = 3; //total users
         if(isset($_POST["calculate"])){
+            $pax = $_POST["userCount"];
             $totalWatt = $_POST["totalWatt"];
             $totalPrice = $_POST["totalPrice"];
             $pricePerUnit = round(pricePerUnit($totalWatt, $totalPrice), 2);
             $_SESSION["startDate"] = $_POST["startDate"];
             $_SESSION["endDate"] = $_POST["endDate"];
-            for($i = 1; $i < 4; $i++){
+            for($i = 1; $i < $pax + 1; $i++){
                 $curUser = "user".$i;
-                $_SESSION[$curUser."Name"] = $_POST[$curUser."Name"];
-                $curUsage = $_POST[$curUser."CurUsage"];
-                $prvUsage = $_POST[$curUser."PrvUsage"];
+                if(isset($_POST[$curUser."Name"])){
+                    $_SESSION[$curUser."Name"] = $_POST[$curUser."Name"];
+                    $curUsage = $_POST[$curUser."CurUsage"];
+                    $prvUsage = $_POST[$curUser."PrvUsage"];
+                }
                 $_SESSION[$curUser."Usage"] = countUsage(floatval($curUsage), floatval($prvUsage));
                 $_SESSION[$curUser."Price"] = countPrice($_SESSION[$curUser."Usage"], $pricePerUnit);
                 $_SESSION["userUsage"] = $_SESSION["userUsage"] + $_SESSION[$curUser."Usage"];
@@ -29,6 +32,22 @@
             $commonArea = countCommonArea(floatval($totalWatt), floatval($_SESSION["userUsage"]), floatval($pricePerUnit));
         }
     ?>
+    <script> 
+    var i=2; 
+    $(document).ready(function(){   
+        $('#add').click(function(){  
+           i++; 
+           document.getElementById("userCount").value = i;
+           $('#dynamic_field').append('<div id="row'+i+'"><div class="form-group row"><label class="col-sm-3 col-form-label">Users Name</label><div class="col-sm-6"><input class="form-control col-sm-11" type="text" name="user'+i+'Name" placeholder="Users Name" required><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove col-sm-1">X</button></div></div><div class="form-group row"><label class="col-sm-3 col-form-label">User Current and Previous Usage</label><div class="col-sm-6"><input class="form-control" type="number" name="user'+i+'PrvUsage" placeholder="Previous Usage (kWh)" required min=1 step=".01"><input class="form-control" type="number" name="user'+i+'CurUsage" placeholder="Current Usage (kWh)" required min=1 step=".01"><br><br></div></div></div>');  
+        }); 
+        $(document).on('click', '.btn_remove', function(){ 
+            i--; 
+            document.getElementById("userCount").value = i;
+            var button_id = $(this).attr("id");   
+            $('#row'+button_id+'').remove();  
+        });  
+    });  
+    </script>      
 </head>
 <body>
 <div class="stepper d-flex flex-column mt-3 ml-2">
@@ -70,6 +89,7 @@
             </div>
         </div>
     </div>
+    <br>
         <div class="d-flex mb-1">
             <div class="d-flex flex-column pr-4 align-items-center">
                 <div class="rounded-circle py-2 px-3 bg-primary text-white mb-1">2</div>
@@ -80,54 +100,49 @@
                 <p class="lead text-muted pb-3">You can get the Readings from the meter.</p>
             </div>
         </div>
-        <!-- <h2>User 1</h2> -->
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label">User's Name</label>
-            <div class="col-sm-6">
-                <input class="form-control" type="text" name="user1Name" placeholder="User's Name" required>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label">User Current and Previous Usage</label>
-            <div class="col-sm-6">
-                <input class="form-control" type="number" name="user1PrvUsage" placeholder="Previous Usage (kWh)" required min=1 step=".01">
-                <input class="form-control" type="number" name="user1CurUsage" placeholder="Current Usage (kWh)" required min=1 step=".01"><br><br>
-            </div>
-        </div>
+        <div class="form-group">  
+            <form name="add_name" id="add_name" method="POST">  
+                <div id="dynamic_field"> 
+                    <div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">User's Name</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" type="text" name="user1Name" placeholder="User's Name" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">User Current and Previous Usage</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" type="number" name="user1PrvUsage" placeholder="Previous Usage (kWh)" required min=1 step=".01">
+                                <input class="form-control" type="number" name="user1CurUsage" placeholder="Current Usage (kWh)" required min=1 step=".01"><br><br>
+                            </div>
+                        </div>
+                    </div> 
 
-        <!-- <h2>Dr Philbert</h2> -->
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label">User's Name</label>
-            <div class="col-sm-6">
-                <input class="form-control" type="text" name="user2Name" placeholder="User's Name" required>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label">User Current and Previous Usage</label>
-            <div class="col-sm-6">
-                <input class="form-control" type="number" name="user2PrvUsage" placeholder="Previous Usage (kWh)" required min=1 step=".01">
-                <input class="form-control" type="number" name="user2CurUsage" placeholder="Current Usage (kWh)" required min=1 step=".01"><br><br>
-            </div>
-        </div>
-
-        <!-- <h2>Dr Ives</h2> -->
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label">User's Name</label>
-            <div class="col-sm-6">
-                <input class="form-control" type="text" name="user3Name" placeholder="User's Name" required>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-3 col-form-label">User Current and Previous Usage</label>
-            <div class="col-sm-6">
-                <input class="form-control" type="number" name="user3PrvUsage" placeholder="Previous Usage (kWh)" required min=1 step=".01">
-                <input class="form-control" type="number" name="user3CurUsage" placeholder="Current Usage (kWh)" required min=1 step=".01"><br><br>
-            </div>
-        </div>
-
-        <input class="btn btn-danger btn-outline-warning" type="submit" name="calculate" value="Calculate Bill">
+                    <div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">User's Name</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" type="text" name="user2Name" placeholder="User's Name" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">User Current and Previous Usage</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" type="number" name="user2PrvUsage" placeholder="Previous Usage (kWh)" required min=1 step=".01">
+                                <input class="form-control" type="number" name="user2CurUsage" placeholder="Current Usage (kWh)" required min=1 step=".01"><br><br>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <input class="btn btn-danger" type="submit" name="calculate" value="Calculate Bill">
+                <button type="button" name="add" id="add" class="btn btn-success">Add Users</button>
+                <input type="hidden" id="userCount" name="userCount" value="2">
+            </form>  
+        </div>    
     </form>
   </div>
+  <br>
   <div class="d-flex mb-1 ml-4 ">
         <div class="d-flex flex-column pr-4 align-items-center">
             <div class="rounded-circle py-2 px-3 bg-primary text-white mb-1">3</div>
@@ -136,13 +151,13 @@
         <div class="col-md-11">
             <h5 class="text-dark">Usages :</h5>
             <p class="lead text-muted pb-3">
-            <?php if(isset($_SESSION["user3Usage"])){ ?>
+            <?php if(isset($_POST["calculate"])){ ?>
                 <div class="container">
                     <table class="table align-self-center">
                     <?php 
                         echo "<p class=".'text-info'.">Price Per Unit: RM ".$pricePerUnit."</p>";
                         if(isset($commonArea)){
-                            echo "<p class=".'text-info'.">Total Common Area: RM ".round(($commonArea / $pax), 2)."</p>";
+                            echo "<p class=".'text-info'.">Total Common Area: RM ".round(($commonArea), 2)."</p>";
                         }
                         ?>
                         <thead>
@@ -165,18 +180,20 @@
                         </thead>
                         <tbody>
                             <?php 
-                            if(isset($_SESSION["user3Usage"])){
-                                for($k = 1; $k < 4; $k++){
+                            if(isset($_POST["calculate"])){
+                                for($k = 1; $k < $pax + 1; $k++){
                                     $curUser = "user".$k;
                                     $totalAmount += round($_SESSION[$curUser."Price"] + ($commonArea / $pax), 2); 
                                     $totalUsage += $_SESSION[$curUser."Usage"];
-                                    echo "<tr>";
-                                        echo "<td>".$_SESSION[$curUser."Name"]."</td>";
-                                        echo "<td>".$_SESSION[$curUser."Usage"]."</td>";
-                                        echo "<td>RM ".round($_SESSION[$curUser."Price"], 2)."</td>";
-                                        echo "<td>RM ".round(($commonArea / $pax), 2)."</td>";
-                                        echo "<td>RM ".round($_SESSION[$curUser."Price"] + ($commonArea / $pax), 2)."</td>";
-                                    echo "</tr>";
+                                    if(isset($_SESSION[$curUser."Name"])){
+                                        echo "<tr>";
+                                            echo "<td>".$_SESSION[$curUser."Name"]."</td>";
+                                            echo "<td>".$_SESSION[$curUser."Usage"]."</td>";
+                                            echo "<td>RM ".round($_SESSION[$curUser."Price"], 2)."</td>";
+                                            echo "<td>RM ".round(($commonArea / $pax), 2)."</td>";
+                                            echo "<td>RM ".round($_SESSION[$curUser."Price"] + ($commonArea / $pax), 2)."</td>";
+                                        echo "</tr>";
+                                    }
                                 }
                             }
                             ?>
